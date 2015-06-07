1950bdd663fd1dc5bc8902bdd556076288799b60
ItzBylkDev <jaydenpatrick1110@gmail.com>
2015-06-07 12:48:29 -0400

    Hopefully no bugs in this

---
+++ src/ItzBulkDev/Main.php
@@ -0,0 +1,43 @@
+<?php
+
+use pocketmine\Player
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\utils\Config;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\utils\TextFormat;
+
+class Main extends PluginBase implements Listener {
+
+public function onEnable(){
+	$this->saveDefaultConfig();
+	$this->getServer()->getPluginManager()->registerEvents($this,$this);
+	$this->getLogger()->info(TextFormat::ORANGE"[JoinOptions] I've been enabled!");
+	$this->getLogger()->info(TextFormat::ORANGE"[JoinOptions] I was created by ItzBulkDev");
+	}
+public function onDisable(){
+	$this->saveDefaultConfig();
+	$this->getLogger()->info(TextFormat::RED"[JoinOptions] I've been disabled!");	
+	}
+	
+	public function onJoin(PlayerJoinEvent $event) {
+	  $cfg = $this->getConfig();
+	  $healthcheck = $cfg->get("Enable-Health-On-Join");
+	  $health = $cfg->get("Set-Health-Amount");
+	  $msg = $cfg->get("Join-PopUp-Message");
+	  $msgcheck = $cfg->get("Enable-PopUp-Messages");
+	  $p = $event->getPlayer()->getName();
+	    if($health == true){
+	      $p->setHealth($health);
+	    }elseif{
+	      return false;
+	      }
+	        if($msgcheck == true){
+	          $p->sendPopup($msg);
+	          }elseif{
+	          return false;
+	          }
+	          
+}
+	          
+	    
